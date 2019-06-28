c04fecf1938d605ddecda23556e07b907db1be69
amirdawd <39911826+amirdawd@users.noreply.github.com>
2019-06-28 16:10:29 +0200

    Create ajax.php

---
+++ ajax.php
@@ -0,0 +1,57 @@
+<?php
+//fetch.php
+include_once 'dbConnection.php';
+$output = '';
+if(isset($_POST["query"]))
+{
+ $search = mysqli_real_escape_string($conn, $_POST["query"]);
+ $query = "
+  SELECT * FROM product
+  WHERE productName LIKE '%".$search."%'
+  OR elfaNumber LIKE '%".$search."%'
+  OR camelNumber LIKE '%".$search."%'
+  OR productType LIKE '%".$search."%'
+  LIMIT 5
+ ";
+}
+else
+{
+ $query = "
+  SELECT * FROM product ORDER BY productName LIMIT 0
+ ";
+}
+$result = mysqli_query($conn, $query);
+if(mysqli_num_rows($result) > 0)
+{
+ $output .= '
+  <div class="table-responsive">
+   <table class="table table bordered">
+    <tr>
+     <th>Name</th>
+     <th>Elfa-ID</th>
+     <th>Camel-ID</th>
+     <th>Type</th>
+    </tr>
+ ';
+ while($row = mysqli_fetch_array($result))
+ {
+  $output .= '
+   <tr>
+    <td>'.$row["productName"].'</td>
+    <td>'.$row["elfaNumber"].'</td>
+    <td>'.$row["camelNumber"].'</td>
+    <td>'.$row["productType"].'</td>
+   </tr>
+  ';
+ }
+ echo $output;
+}
+if(!($row = mysqli_fetch_array($result)))
+{
+ echo '';
+}
+else{
+  echo 'Data Not Found :(';
+}
+
+?>
